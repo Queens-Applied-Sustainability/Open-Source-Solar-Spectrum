f9aa7ae157eeab045ffcdd2cf0eafe233d7037c6
Philip Schleihauf <uniphil@gmail.com>
2012-05-08 13:39:29 -0400

    Style, consistency, refactoring.
    No functional difference

--- stepper_control/stepper_control.ino
+++ stepper_control/stepper_control.ino
@@ -4,6 +4,20 @@
  * 
  * Based on code originally written by Matthew De Vuono
  */
+ 
+///////////// INSTALLATION-SPECIFIC CONSTANTS
+#define LATITUDE 43.0  
+#define LONGITUDE 76.3
+#define MERIDIAN 75.0
+
+// Global Constants
+#define SPEC_TRIGGER 13 // output pin to trigger a spectrometer reading
+#define BUTTON 2
+
+// External Libraries
+#include <Wire.h>
+#include <AFMotor.h>   // http://www.ladyada.net/make/mshield/download.html
+#include <Chronodot.h> // http://planetstephanie.net/2011/04/09/chronodot-library-for-arduino/
 
 
 //this code merges the zenith calculations with the stepper motion in
@@ -11,119 +25,110 @@
 //results from the code. The steppers will move in 10 degree increments to 90degrees.
 //includes code to initialize the position of the stepper motor for location calibration.
 
-#include <Wire.h>
-#include <AFMotor.h>     // http://www.ladyada.net/make/mshield/download.html
-#include <Chronodot.h>   // http://planetstephanie.net/2011/04/09/chronodot-library-for-arduino/
 
-Chronodot RTC;
+Chronodot RTC; // Real-time Clock
 
-const float pi = 3.14592654;
+AF_Stepper optic_motor(200, 1);  // M1 and M2 on the motor driving sheild
+AF_Stepper band_motor(200, 2);   // M3 and M4
 
-AF_Stepper motor1(200, 1);     //'1' for M1 and M2 or '2' for M3 and M4
-AF_Stepper motor2(200, 2);
+// fixme: these should not be global
 long previousMillis = 0;      // will store last time LED was updated
-long interval = 58;           // interval at which to blink (milliseconds)
-int montharray[13]={
-  0,1,2,3,4,5,6,7,8,9,10,11,12};
-int hourarray[25]={
-  0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,
-  19,20,21,22,23,24};    //hour (1-24)
+
 
 //array to hold the values to find day of year
-int monthdoy[13] = {
-  0,0,31,59,90,120,151,181,212,243,273,304,334};   
-float latitude = 43;    //user changes per location
-float longitude = 76.3;    //user changes per location
-float meridian = 75;    //user changes per location
+int month_day_of_year[13] = {
+  0,0,31,59,90,120,151,181,212,243,273,304,334}; 
 float phi;    //initialize latitude in radians
-int buttonstate = 0;
-const int buttonpin = 2;
+int button_state = 0;
+
+void scratch(DateTime &now);
 
 void setup() {
   // initialize serial output
   Serial.begin(9600);
   Wire.begin();
   RTC.begin();
-  motor1.setSpeed(10);  // 10 rpm 
-  motor2.setSpeed(10);  // 10 rpm  
-  pinMode(13, OUTPUT);
-  buttonstate = digitalRead(buttonpin);
-  pinMode(buttonpin, INPUT);    
-  phi = (latitude*pi)/180;     //latitude to radians
+  optic_motor.setSpeed(10);  // 10 rpm 
+  band_motor.setSpeed(10);  // 10 rpm  
+  pinMode(SPEC_TRIGGER, OUTPUT);
+  button_state = digitalRead(BUTTON);
+  pinMode(BUTTON, INPUT);
 }
 
-void loop() {   
+void loop() {
   DateTime now = RTC.now();
   int time = now.minute();
 
   if(time = 00) {
   //the instances for the program to run, in this case every minute
-    // initialize motors
-    if (buttonstate == LOW) {     
-      // move motor 
-      motor1.step(1, FORWARD, SINGLE); 
-      motor2.step(1, FORWARD, SINGLE);  
-    }
-
-    //run sensing program
-    for(int i=0; i<10; i=i++) {
-      motor1.step(5, FORWARD, DOUBLE);
-      delay(5000);
-      digitalWrite(13, HIGH);   // set the spectrometer on
-      delay(250);              // wait for a second
-      digitalWrite(13, LOW);
-      delay(2000);
-    }
-    motor1.step(50, BACKWARD, DOUBLE);
-
-    //check and import clock values from rtc clock
-
-    int hour = now.hour();          //from clock
-    int day = now.day();        //from clock
-    int month = now.month();      //from clock
-
-    //calculating the day of year
-    int doy = monthdoy[month] + day;
-
-    //declination calculations
-    float ratio = (284.0+doy)/365*360;
-    float ratiorad = ratio*pi/180.0;
-    float decdeg = 23.45*sin(ratiorad);
-    float dec = decdeg*pi/180;
-
-    //calculating solar time
-    int Bdeg = (doy-1)*(360/365);
-    float B = Bdeg*pi/180;                 //dividing E by 60 gives it in a fraction of an hour
-    float E = (229.2*(0.000075+0.001868*cos(B)-0.032077*sin(B)-0.014615*cos(2*B)-0.04089*sin(2*B)))/60;
-    float solartime = hour + 4*(meridian-longitude)/60+E;
-
-    //solar hour calculation
-    float omegadegrees=(solartime-12)*15;
-    float omegarad =omegadegrees*pi/180;
-
-    //Zeith calculation in radians
-    float zenith =cos(phi)*cos(dec)*cos(omegarad)+sin(phi)*sin(dec);
-    zenith = (acos(zenith))*180/pi;
-
-
-    float result = ((90-zenith)/1.8);
-    int angle = result;
-
-    motor2.step(angle, FORWARD, DOUBLE);
-    delay(5000);
-    for(int i=0; i<10; i++) { 
-      motor1.step(5, FORWARD, DOUBLE);
-      delay(5000);
-      digitalWrite(13, HIGH);   // set the spectrometer on
-      delay(250);              // wait for a second
-      digitalWrite(13, LOW);
-      delay(2000);
-    }
-    motor1.step(50, BACKWARD, DOUBLE);
-    motor2.step(angle, BACKWARD, DOUBLE);
+    scratch(now);
   }
-  motor1.release();
-  motor2.release();
-
+  optic_motor.release();
+  band_motor.release();
 }
 
+void scratch(DateTime &now) {
+ // initialize motors
+  if (button_state == LOW) {     
+    // move motor 
+    optic_motor.step(1, FORWARD, SINGLE); 
+    band_motor.step(1, FORWARD, SINGLE);  
+  }
+
+  //run sensing program
+  for(int i=0; i<10; i=i++) {
+    optic_motor.step(5, FORWARD, DOUBLE);
+    delay(5000);
+    digitalWrite(SPEC_TRIGGER, HIGH);   // set the spectrometer on
+    delay(250);              // wait for a moment
+    digitalWrite(SPEC_TRIGGER, LOW);
+    delay(2000);
+  }
+  optic_motor.step(50, BACKWARD, DOUBLE);
+
+  //check and import clock values from rtc clock
+
+  int hour = now.hour();          //from clock
+  int day = now.day();        //from clock
+  int month = now.month();      //from clock
+
+  //calculating the day of year
+  int day_of_year = month_day_of_year[month] + day;
+
+  //declination calculations
+  float ratio = (284.0+day_of_year)/365*360;
+  float ratio_rad = ratio*PI/180.0;
+  float dec_deg = 23.45*sin(ratio_rad);
+  float dec = dec_deg*PI/180;
+
+  //calculating solar time
+  int Bdeg = (day_of_year-1)*(360/365);
+  float B = Bdeg*PI/180;          //dividing E by 60 gives it in a fraction of an hour
+  float E = (229.2*(0.000075+0.001868*cos(B)-0.032077*sin(B)-0.014615*cos(2*B)-0.04089*sin(2*B)))/60;
+  float solartime = hour + 4*(MERIDIAN-LONGITUDE)/60+E;
+
+  //solar hour calculation
+  float omega_degrees=(solartime-12)*15;
+  float omega_rad =omega_degrees*PI/180;
+
+  //Zeith calculation in radians
+  float zenith =cos(phi)*cos(dec)*cos(omega_rad)+sin(phi)*sin(dec);
+  zenith = (acos(zenith))*180/PI;
+
+
+  float result = ((90-zenith)/1.8);
+  int angle = result;
+
+  band_motor.step(angle, FORWARD, DOUBLE);
+  delay(5000);
+  for(int i=0; i<10; i++) { 
+    optic_motor.step(5, FORWARD, DOUBLE);
+    delay(5000);
+    digitalWrite(SPEC_TRIGGER, HIGH);   // set the spectrometer on
+    delay(250);              // wait for a moment
+    digitalWrite(SPEC_TRIGGER, LOW);
+    delay(2000);
+  }
+  optic_motor.step(50, BACKWARD, DOUBLE);
+  band_motor.step(angle, BACKWARD, DOUBLE);
+}
